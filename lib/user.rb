6d947d5e34f5df69cb66f22e7c0f90bac77d30d0
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-08-23 17:10:08 +0300

    commit tree 0137a6295a39d7680c2342b94ccf71fdaaa2755c

---
+++ lib/user.rb
@@ -0,0 +1,7 @@
+# Define the User class such that a user can have a first and last name. 
+# You'll need both a setter and a getter for first and last name
+class User
+
+    attr_accessor :first_name, :last_name
+  
+  end
